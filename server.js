a149bcf0e0b0ada7c901d4e43f0a6161de1b7c30
AnuragSingh8090 <anuragkumarsingh154@gmail.com>
2026-07-31 01:31:38 +0530

    some changes

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ import cors from 'cors';
 import recordRoutes from './routes/recordRoutes.js';
 
 const app = express();
-const PORT = process.env.PORT || 5000;
+const PORT = 5000;
 
 // Middleware
 app.use(cors());
